3ac845f1e250a0d1b518c1e5420891d990541dd7
amogus <devmalcevv@gmail.com>
2025-10-18 12:46:37 +0400

    Finished 69: Sqrt(x)

---
+++ solutions/69/solution.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func mySqrt(x int) int {
+	l, r := 0, x
+	for l < r {
+		mid := (l + r + 1) / 2
+		if mid*mid > x {
+			r = mid - 1
+		} else {
+			l = mid
+		}
+	}
+	return l
+}
+
+func mySqrt1(x int) int {
+	r := x
+	for r*r > x {
+		r = (r + x/r) / 2
+	}
+	return r
+}
