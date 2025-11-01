319455e78bde5f5edac1801c16b758c929a73faf
amogus <devmalcevv@gmail.com>
2025-11-01 12:49:09 +0400

    Finished 503: Next Greater Element II

---
+++ solutions/503/solution.go
@@ -0,0 +1,41 @@
+package leetcode
+
+func nextGreaterElements(nums []int) []int {
+	res := make([]int, 0)
+	indexes := make([]int, 0)
+	for i := 0; i < len(nums); i++ {
+		res = append(res, -1)
+	}
+	for i := 0; i < len(nums)*2; i++ {
+		num := nums[i%len(nums)]
+		for len(indexes) > 0 && nums[indexes[len(indexes)-1]] < num {
+			index := indexes[len(indexes)-1]
+			res[index] = num
+			indexes = indexes[:len(indexes)-1]
+		}
+		indexes = append(indexes, i%len(nums))
+	}
+	return res
+}
+
+func nextGreaterElements1(nums []int) []int {
+	if len(nums) == 0 {
+		return []int{}
+	}
+	res := []int{}
+	for i := 0; i < len(nums); i++ {
+		j, find := (i+1)%len(nums), false
+		for j != i {
+			if nums[j] > nums[i] {
+				find = true
+				res = append(res, nums[j])
+				break
+			}
+			j = (j + 1) % len(nums)
+		}
+		if !find {
+			res = append(res, -1)
+		}
+	}
+	return res
+}
