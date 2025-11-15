8f487f4fb37a8f8164c4c678de76947179c474d5
amogus <devmalcevv@gmail.com>
2025-11-15 13:46:57 +0400

    Finished 206: Reverse Linked List

---
+++ solutions/206/solution.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import (
+	"leetcode-solutions/structures"
+)
+
+type ListNode = structures.ListNode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+	var behind *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = behind
+		behind = head
+		head = next
+	}
+	return behind
+}
