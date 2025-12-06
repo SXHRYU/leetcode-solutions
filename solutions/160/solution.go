35850ec754db4c2c8df45c4cd1041e6d466e3e33
amogus <devmalcevv@gmail.com>
2025-12-06 13:21:57 +0400

    Finished 160: Intersection of Two Linked Lists

---
+++ solutions/160/solution.go
@@ -0,0 +1,43 @@
+package leetcode
+
+import (
+	"fmt"
+
+	"leetcode-solutions/structures"
+)
+
+type ListNode = structures.ListNode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	a := headA
+	b := headB
+
+	for a != b {
+
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
+
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+		fmt.Printf("a = %v b = %v\n", a, b)
+	}
+	return a
+}
