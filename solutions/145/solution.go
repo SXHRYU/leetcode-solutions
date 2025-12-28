e5c484fc5a9b0637ec2a19d2e40fc20f477e622a
amogus <devmalcevv@gmail.com>
2025-12-28 12:41:40 +0400

    Finished 145: Binary Tree Postorder Traversal

---
+++ solutions/145/solution.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"leetcode-solutions/structures"
+)
+
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func postorderTraversal(root *TreeNode) []int {
+	var result []int
+	postorder(root, &result)
+	return result
+}
+
+func postorder(root *TreeNode, output *[]int) {
+	if root != nil {
+		postorder(root.Left, output)
+		postorder(root.Right, output)
+		*output = append(*output, root.Val)
+	}
+}
