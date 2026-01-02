a7b373c2d7c35b8fa3e56f5fd935dd8fdafd0023
amogus <devmalcevv@gmail.com>
2026-01-02 12:53:46 +0400

    Finished 700: Search in a Binary Search Tree

---
+++ solutions/700/solution.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"leetcode-solutions/structures"
+)
+
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	} else if root.Val < val {
+		return searchBST(root.Right, val)
+	} else {
+		return searchBST(root.Left, val)
+	}
+}
