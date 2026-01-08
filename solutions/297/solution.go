a9d2599b0891e2332adabdbfa68ee2a141ba2843
amogus <devmalcevv@gmail.com>
2026-01-08 13:30:33 +0400

    Finished 297: Serialize and Deserialize Binary Tree

---
+++ solutions/297/solution.go
@@ -0,0 +1,52 @@
+package leetcode
+
+import (
+	"strconv"
+	"strings"
+
+	"leetcode-solutions/structures"
+)
+
+type TreeNode = structures.TreeNode
+
+type Codec struct {
+	builder strings.Builder
+	input   []string
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+func (this *Codec) serialize(root *TreeNode) string {
+	if root == nil {
+		this.builder.WriteString("#,")
+		return ""
+	}
+	this.builder.WriteString(strconv.Itoa(root.Val) + ",")
+	this.serialize(root.Left)
+	this.serialize(root.Right)
+	return this.builder.String()
+}
+
+func (this *Codec) deserialize(data string) *TreeNode {
+	if len(data) == 0 {
+		return nil
+	}
+	this.input = strings.Split(data, ",")
+	return this.deserializeHelper()
+}
+
+func (this *Codec) deserializeHelper() *TreeNode {
+	if this.input[0] == "#" {
+		this.input = this.input[1:]
+		return nil
+	}
+	val, _ := strconv.Atoi(this.input[0])
+	this.input = this.input[1:]
+	return &TreeNode{
+		Val:   val,
+		Left:  this.deserializeHelper(),
+		Right: this.deserializeHelper(),
+	}
+}
