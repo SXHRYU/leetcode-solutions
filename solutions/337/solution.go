c028e15412bb0726bd300a46eccce91a82eb61fa
amogus <devmalcevv@gmail.com>
2026-09-12 12:25:07 +0400

    Finished 337: House Robber III

---
+++ solutions/337/solution.go
@@ -0,0 +1,40 @@
+package leetcode
+
+import (
+	"leetcode-solutions/structures"
+)
+
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rob337(root *TreeNode) int {
+	a, b := dfsTreeRob(root)
+	return max(a, b)
+}
+
+func dfsTreeRob(root *TreeNode) (a, b int) {
+	if root == nil {
+		return 0, 0
+	}
+	l0, l1 := dfsTreeRob(root.Left)
+	r0, r1 := dfsTreeRob(root.Right)
+
+	tmp0 := max(l0, l1) + max(r0, r1)
+
+	tmp1 := root.Val + l0 + r0
+	return tmp0, tmp1
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
